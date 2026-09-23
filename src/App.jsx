bfbc6bc25a3225bde4678a3bdbaec279c9090c70
OmarHegazy1995 <omar_hegazy@cic-cairo.com>
2026-09-23 12:59:07 +0200

    Add scroll to top on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,6 +14,7 @@ import Wishlist from './pages/Wishlist';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
